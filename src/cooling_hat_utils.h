2586fb383568af1e717d3d4258b488dc55dc5028
Michal Duda <github@vookimedlo.cz>
2020-10-15 20:48:51 +0200

    🔩Code: __VA__OPT__ in debug printing

--- src/cooling_hat_utils.h
+++ src/cooling_hat_utils.h
@@ -6,7 +6,7 @@
 #else
     #include <stdio.h>
     #define DEBUG_PRINT(FORMAT, ...) \
-        do { fprintf(stderr, FORMAT "\n",  __VA_ARGS__); } while(0)
+        do { fprintf(stderr, FORMAT "\n" __VA_OPT__(,)  __VA_ARGS__); } while(0)
 #endif // DEBUG
 
 #endif //COOLING_UTILS_H
